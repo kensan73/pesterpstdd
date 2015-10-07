39528605f259bf8f7cd14d41e913807d92901e61
kensan73 <ken.60625@gmail.com>
2015-10-06 22:38:00 -0500

    first checkin

---
+++ Unzip-StudioFiles.Tests.ps1
@@ -0,0 +1,57 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\$sut"
+
+Describe "Unzip-StudioFiles" {
+    Function Does-FolderExist { param ($Path) }
+	Function Log-Info { param ($Message) }
+	Function Log-Debug { param ($Message) }
+	Function Log-Error { param ($Message) }
+
+    It "prints usage on 0 or 1 parameters" {
+		$usage = "Usage: unzip-studiofiles \\janus\studio_work \\janus\studio_work"
+		
+		Unzip-StudioFiles | Should Be $usage
+		Unzip-StudioFiles "paramone" | Should Be $usage
+    }
+
+    It "fails on nonexistent source folder" {
+		$source = "\\somesource\with_folder"
+		$dest = "\\somedest\another_folder"
+		$sourcenonexistent = "Source does not exist, please check now exiting: " + $source
+		Mock Does-FolderExist –ParameterFilter { $Path –eq $source } -MockWith { $false }
+		Mock Log-Error -ParameterFilter { $Message -eq $sourcenonexistent }
+		
+		Unzip-StudioFiles $source $dest # | Should Be $sourcenonexistent
+		
+		Assert-MockCalled Log-Error –ParameterFilter { $Message -eq $sourcenonexistent } -Times 1
+		Assert-MockCalled Does-FolderExist –ParameterFilter { $Path -eq $source } -Times 1
+    }
+
+    It "fails on nonexistent dest folder" {
+		$source = "\\somesource\with_folder"
+		$dest = "\\somedest\another_folder"
+		$destnonexistent = "Dest does not exist, please check now exiting: " + $dest
+		Mock Does-FolderExist –ParameterFilter { $Path –eq $source } -MockWith { $true }
+		Mock Does-FolderExist –ParameterFilter { $Path –eq $dest } -MockWith { $false }
+		Mock Log-Error -ParameterFilter { $Message -eq $destnonexistent }
+		
+		Unzip-StudioFiles $source $dest #| Should Be $destnonexistent
+		
+		Assert-MockCalled Log-Error –ParameterFilter { $Message -eq $destnonexistent } -Times 1
+		Assert-MockCalled Does-FolderExist –ParameterFilter { $Path -eq $source } -Times 1
+		Assert-MockCalled Does-FolderExist –ParameterFilter { $Path -eq $dest } -Times 1
+    }
+
+    It "no zips found exits" {
+		$source = "\\somesource\with_folder"
+		$dest = "\\somedest\another_folder"
+		Mock Does-FolderExist –ParameterFilter { $Path –eq $source } -MockWith { $true }
+		Mock Does-FolderExist –ParameterFilter { $Path –eq $dest } -MockWith { $true }
+		
+		Unzip-StudioFiles $source $dest
+		
+		Assert-MockCalled Does-FolderExist –ParameterFilter { $Path -eq $source } -Times 1
+		Assert-MockCalled Does-FolderExist –ParameterFilter { $Path -eq $dest } -Times 1
+    }
+}
---
+++ Unzip-StudioFiles.ps1
@@ -0,0 +1,19 @@
+﻿function Unzip-StudioFiles {
+	if($args.Count -lt 2){
+		"Usage: unzip-studiofiles \\janus\studio_work \\janus\studio_work"
+		return
+	}
+	
+	$source = $args[0]
+	$dest = $args[1]
+	if((Does-FolderExist -Path $source) -eq $false){
+		$message = "Source does not exist, please check now exiting: " + $source
+		Log-Error $message
+		return
+	}
+	if((Does-FolderExist -Path $dest) -eq $false){
+		$message = "Dest does not exist, please check now exiting: " + $dest
+		Log-Error $message
+		return
+	}
+}
